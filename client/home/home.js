1e4dcebfa26eb7e2c4f2ecd96c8eb5a9875a7de5
Justin Pinili <pinili.justin@gmail.com>
2014-10-13 18:37:14 -0700

    corrected reset button

--- client/home/home.js
+++ client/home/home.js
@@ -3,6 +3,15 @@ angular.module('sundayone.home', [])
 .controller('HomeController', function ($scope, $window, $location, $state) {
   $scope.user = "justin";
 
+  $scope.todo = {};
+
+  $scope.todos = [];
+
+  $scope.addTodos = function() {
+    $scope.todos.push( $scope.todo );
+    $scope.todo = {};
+  };
+
   $state.transitionTo('home.subviews');
 
 });
